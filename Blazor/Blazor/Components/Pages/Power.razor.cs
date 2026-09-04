267f78bfe9ebd3bc19f2f88f471aab6833252747
kit <kit@DESKTOP-EHKDJE1>
2026-09-04 20:53:14 +0300

    pover_1

--- Blazor/Blazor/Components/Pages/Power.razor.cs
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -9,21 +9,11 @@ namespace Blazor.Components.Pages
          double a = 1;
          int b = 1;
          double result = 1;
-        private bool isBabaFlying = false;
 
-        private async Task CalculateAsync()
+        private void Calculate()
         {
             result = Math.Pow(a, b);
 
-            // 1. Убираем класс
-            isBabaFlying = false;
-            StateHasChanged(); // Принудительно просим Blazor обновить DOM
-
-            // 2. Ждем 1 кадр (минимум 16мс, ставим 50мс для надежности)
-            await Task.Delay(50);
-
-            // 3. Добавляем класс обратно -> Браузер видит изменение и запускает animation
-            isBabaFlying = true;
         }
 
     }
