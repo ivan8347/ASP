062de174e6bf706f60b51bb3ca260036c3d2c7d6
kit <kit@DESKTOP-EHKDJE1>
2026-09-04 20:41:08 +0300

    Power

---
+++ Blazor/Blazor/Components/Pages/Fibonacci.razor.cs
@@ -0,0 +1,53 @@
+﻿/*using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+	public partial class Fibonacci
+	{
+		int a = 0, b = 1, c = 0, i = 0, n = 0;
+		//       cout << "Введите число :";
+
+		//cin >> n;
+		//cout << "Ряд Фибоначи числа:" << n << endl;
+		void Calculate()
+		{
+			while (i < n)
+			{
+				a = b;
+				b = c;
+				c = a + b;
+
+				//	cout << "\t" << c << endl;
+				i++;
+			}
+		}
+	}
+}*/
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Fibonacci
+    {
+        private int n = 0;
+        private List<BigInteger> F = new();
+
+        private void Calculate()
+        {
+            F.Clear();
+            if (n <= 0) return;
+
+            BigInteger a = 0, b = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                F.Add(a);
+                BigInteger next = a + b;
+                a = b;
+                b = next;
+            }
+        }
+    }
+}
+
---
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+
+
+namespace Blazor.Components.Pages
+{
+    public partial class Power
+    {
+         double a = 1;
+         int b = 1;
+         double result = 1;
+        private bool isBabaFlying = false;
+
+        private async Task CalculateAsync()
+        {
+            result = Math.Pow(a, b);
+
+            // 1. Убираем класс
+            isBabaFlying = false;
+            StateHasChanged(); // Принудительно просим Blazor обновить DOM
+
+            // 2. Ждем 1 кадр (минимум 16мс, ставим 50мс для надежности)
+            await Task.Delay(50);
+
+            // 3. Добавляем класс обратно -> Браузер видит изменение и запускает animation
+            isBabaFlying = true;
+        }
+
+    }
+}
