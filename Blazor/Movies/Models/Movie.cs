d3680ae040fd7ba0e17462fb32bbab12153e1110
kit <kit@DESKTOP-EHKDJE1>
2026-09-12 12:13:21 +0300

    Lesson 5 CRUD in progress

--- Blazor/Movies/Models/Movie.cs
+++ Blazor/Movies/Models/Movie.cs
@@ -6,7 +6,7 @@
         public string Title { get; set; }
         public DateOnly ReleaseDate {  get; set; }
         public string Genre { get; set; }
-        public string URL {  get; set; }
-        public string Poster {  get; set; }
+        public string? URL {  get; set; }
+        public string? Poster {  get; set; }
     }
 }
